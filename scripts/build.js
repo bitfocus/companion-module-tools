bitfocus/companion-module-tools

commit 2a4e3115ca26c695d409080d07ef8ad5e6dd840a

feat: add option to build with support for node-gyp-build (#24)

scripts/build.js
@@ -30,12 +30,17 @@ console.log(`Framework path: ${frameworkDir}`)
 // clean old
 await fs.remove('pkg')
 
-const webpackArgs = []
-if (argv.dev) webpackArgs.push('--env', 'dev')
+const webpackArgs = {}
+if (argv.dev || argv.debug) webpackArgs['env']= 'dev'
+
+const webpackArgsArray = []
+for (const [k, v] of Object.entries(webpackArgs)) {
+	webpackArgsArray.push(`--${k}`, v)
+}
 
 // build the code
 const webpackConfig = path.join(toolsDir, 'webpack.config.cjs').replace(/\\/g, '/') // Fix slashes because windows is a pain
-await $`yarn webpack -c ${webpackConfig} ${webpackArgs}`
+await $`yarn webpack -c ${webpackConfig} ${webpackArgsArray}`
 
 // copy in the metadata
 await fs.copy('companion', 'pkg/companion')
@@ -84,7 +89,7 @@ if (fs.existsSync(webpackExtPath)) {
 				for (const external of Object.keys(extGroup)) {
 					const extPath = await findUp('package.json', { cwd: require.resolve(external) })
 					const extJson = JSON.parse(await fs.readFile(extPath))
-					packageJson.dependencies[extJson.name] = extJson.version
+					packageJson.dependencies[external] = extJson.version
 				}
 			}
 		}
@@ -113,6 +118,37 @@ if (fs.existsSync(webpackExtPath)) {
 		}
 	}
 }
+
+// Copy node-gyp-build prebulds
+const webpackConfigJson = await require(webpackConfig)(webpackArgs)
+if (webpackConfigJson.node?.__dirname === true) {
+	const copyNodeGypBuildPrebuilds = (thisPath) => {
+		const nodeModPath = path.join(thisPath, 'node_modules')
+		if (fs.existsSync(nodeModPath)) {
+			for (const dir of fs.readdirSync(nodeModPath)) {
+				const modDir = path.join(nodeModPath, dir)
+				copyNodeGypBuildPrebuilds(modDir)
+			}
+		}
+
+		const pkgJsonPath = path.join(thisPath, 'package.json')
+		if (thisPath && fs.existsSync(pkgJsonPath)) {
+			const dirPkgJsonStr = fs.readFileSync(pkgJsonPath)
+			const dirPkgJson = JSON.parse(dirPkgJsonStr.toString())
+
+			const prebuildsDir = path.join(thisPath, 'prebuilds')
+			if (dirPkgJson.dependencies?.['node-gyp-build'] && fs.existsSync(prebuildsDir)) {
+				fs.mkdirpSync(path.join('pkg', thisPath))
+				fs.copySync(prebuildsDir, path.join('pkg', prebuildsDir))
+				
+				console.log('copying node-gyp-build prebuilds from', thisPath)
+			}
+		}
+	}
+	
+	copyNodeGypBuildPrebuilds('')
+}
+
 
 // Write the package.json
 // packageJson.bundleDependencies = Object.keys(packageJson.dependencies)

webpack.config.cjs
@@ -58,5 +58,8 @@ module.exports = async (env) => {
 			// Let modules define additional plugins. Hopefully this won't conflict with anything we add
 			...(webpackExt.plugins || []),
 		],
+		node: webpackExt.useOriginalStructureDirname ? {
+			__dirname: true
+		} : undefined,
 	}
 }

commit 339d2acd6cd5d8a770766214d0687a7c9fd05196

fix: use name from package.json when building externals dependencies object
Previously implemented by 6563d0e1b23123a056146576e7721c3e2f9e4cfd (v1.2.1) and broken by 2a4e3115ca26c695d409080d07ef8ad5e6dd840a (v1.3.0)

scripts/build.js
@@ -94,7 +94,7 @@ if (fs.existsSync(webpackExtPath)) {
 				for (const external of Object.keys(extGroup)) {
 					const extPath = await findUp('package.json', { cwd: require.resolve(external) })
 					const extJson = JSON.parse(await fs.readFile(extPath))
-					packageJson.dependencies[external] = extJson.version
+					packageJson.dependencies[extJson.name] = extJson.version
 				}
 			}
 		}