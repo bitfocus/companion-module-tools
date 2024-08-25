23e81bf5799f56fa5c27e65aa6c292aa6c414a40
Julian Waller <git@julusian.co.uk>
2024-08-25 01:18:13 +0100

    fix: remove old eslint configs

--- eslint/main.cjs
+++
@@ -1,6 +0,0 @@
-const generateEslintConfig = require('./index.cjs')
-
-/**
- * @deprecated use the line like below to generate your own config
- */
-module.exports = generateEslintConfig({ enableTypescript: true })
