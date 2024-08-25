45615fc3d5448d32ea37b9023e9476ca4893c407
Julian Waller <git@julusian.co.uk>
2024-08-25 01:38:42 +0100

    feat!: remove eslint and prettier as dependencies, modules should install these directly (#43)

--- README.md
+++ README.md
@@ -13,3 +13,58 @@ More information on this command is available [on the wiki](https://github.com/b
 ### companion-generate-manifest
 
 Generate the new format manifest from an old package.json
+
+
+## Upgrading from v1.x to v2.0
+
+v2.0 of this library includes some breaking changes to how eslint and prettier handled.
+Instead of it being installed as a dependency, and being directly usable by modules it has to be explicitly installed by modules.
+
+This change was done for two reasons:
+1) Very few modules use eslint, making this extra weight for them for no gain
+2) Recent versions of yarn do not expose these binaries in a way which is easily callable by modules, requiring tricks to be able to execute them.
+
+To resolve this, you will need to do a `yarn add --dev eslint prettier` in your modules to install the dependencies, and update any scripts to remove the invocation hacks.
+
+This also allows for eslint to be updated to v9, which requires a new config format. This unfortonately means that your config file needs to be replaced.
+
+A new basic config should be called `eslint.config.mjs` (remove any existing `.eslintrc.json` or `.eslintrc.cjs`), and could contain:
+```js
+import { generateEslintConfig } from '@companion-module/tools/eslint/config.mjs'
+
+export default generateEslintConfig({})
+```
+
+If using typescript, you should specify a `typescriprRoot`
+
+```js
+import { generateEslintConfig } from '@companion-module/tools/eslint/config.mjs'
+
+export default generateEslintConfig({
+    typescriptRoot: '.',
+})
+```
+
+You can easily override rules in this setup with:
+```js
+import { generateEslintConfig } from '@companion-module/tools/eslint/config.mjs'
+
+const baseConfig = await generateEslintConfig({
+	typescriptRoot: '.',
+})
+
+const customConfig = [
+	...baseConfig,
+
+	{
+		rules: {
+			'n/no-missing-import': 'off',
+			'node/no-unpublished-import': 'off',
+		},
+	},
+]
+
+export default customConfig
+```
+
+More options are available for the `generateEslintConfig` function, check the wiki or the method signature to see what is available.
---
+++ eslint/config.mjs
@@ -0,0 +1,139 @@
+// @ts-check
+
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import eslint from '@eslint/js'
+import neslint from 'eslint-plugin-n'
+
+/**
+ *
+ * @template T
+ * @param {Record<string, T | null | undefined>} obj
+ * @returns {Record<string, T>}
+ */
+function compactObj(obj) {
+	/** @type {Record<string, T>} */
+	const result = {}
+
+	for (const [key, value] of Object.entries(obj)) {
+		if (value) result[key] = value
+	}
+
+	return result
+}
+
+/***
+ * @param {{ enableJest?: boolean, typescriptRoot?: string, ignores?: string[] }} [options={}] - Options to customize the config
+ * @returns {Promise<import('eslint').Linter.Config[]>}
+ */
+export async function generateEslintConfig(options = {}) {
+	const tseslint = options.typescriptRoot ? await import('typescript-eslint') : null
+
+	/** @type {import('eslint').Linter.Config} */
+	const result = {
+		// extends: commonExtends,
+		plugins: compactObj({
+			'@typescript-eslint': tseslint ? tseslint.plugin : null,
+		}),
+		rules: {
+			// Default rules to be applied everywhere
+			'prettier/prettier': 'error',
+
+			...eslint.configs.recommended.rules,
+
+			'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_(.+)' }],
+			'no-extra-semi': 'off',
+			// 'n/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }],
+			'no-use-before-define': 'off',
+			'no-warning-comments': ['error', { terms: ['nocommit', '@nocommit', '@no-commit'] }],
+			// 'jest/no-mocks-import': 'off',
+		},
+	}
+
+	return [
+		// setup the parser first
+		tseslint
+			? {
+					languageOptions: {
+						parser: tseslint.parser,
+						parserOptions: {
+							project: true,
+						},
+					},
+				}
+			: null,
+
+		neslint.configs['flat/recommended-script'],
+		result,
+		...(tseslint ? tseslint.configs.recommendedTypeChecked : []),
+		{
+			// disable type-aware linting on JS files
+			files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
+			...(tseslint ? tseslint.configs.disableTypeChecked : {}),
+		},
+		tseslint
+			? {
+					files: ['**/*.ts', '**/*.cts', '**/*.mts'],
+					rules: {
+						'@typescript-eslint/no-explicit-any': 'off',
+						'@typescript-eslint/interface-name-prefix': 'off',
+						'@typescript-eslint/no-unused-vars': [
+							'error',
+							{ argsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_', varsIgnorePattern: '^_(.+)' },
+						],
+						'@typescript-eslint/no-floating-promises': 'error',
+						'@typescript-eslint/explicit-module-boundary-types': ['error'],
+						'@typescript-eslint/promise-function-async': 'error',
+						'@typescript-eslint/require-await': 'off', // conflicts with 'promise-function-async'
+
+						/** Disable some annoyingly strict rules from the 'recommended-requiring-type-checking' pack */
+						'@typescript-eslint/no-unsafe-assignment': 0,
+						'@typescript-eslint/no-unsafe-member-access': 0,
+						'@typescript-eslint/no-unsafe-argument': 0,
+						'@typescript-eslint/no-unsafe-return': 0,
+						'@typescript-eslint/no-unsafe-call': 0,
+						'@typescript-eslint/restrict-template-expressions': 0,
+						'@typescript-eslint/restrict-plus-operands': 0,
+						'@typescript-eslint/no-redundant-type-constituents': 0,
+						/** End 'recommended-requiring-type-checking' overrides */
+					},
+				}
+			: null,
+		tseslint
+			? {
+					files: ['**/__tests__/**/*', 'test/**/*'],
+					rules: {
+						'@typescript-eslint/ban-ts-ignore': 'off',
+						'@typescript-eslint/ban-ts-comment': 'off',
+					},
+				}
+			: null,
+		{
+			// disable type-aware linting on JS files
+			files: [
+				'examples/**/*.js',
+				'examples/**/*.cjs',
+				'examples/**/*.mjs',
+				'examples/**/*.ts',
+				'examples/**/*.cts',
+				'examples/**/*.mts',
+			],
+			rules: {
+				'no-process-exit': 'off',
+				'n/no-missing-import': 'off',
+			},
+		},
+
+		// Add prettier at the end to give it final say on formatting
+		eslintPluginPrettierRecommended,
+		{
+			// But lastly, ensure that we ignore certain paths
+			ignores: ['**/dist/*', '/dist', '**/pkg/*', '**/docs/*', '**/generated/*', ...(options.ignores || [])],
+		},
+		{
+			files: ['eslint.config.*'],
+			rules: {
+				'n/no-unpublished-import': 'off',
+			},
+		},
+	].filter((v) => !!v)
+}
